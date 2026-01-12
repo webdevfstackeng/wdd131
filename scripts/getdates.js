46d0f052cf419d4341f4fb37953af5337f8f5188
webdevfstackeng <u175797830@byui.edu>
2026-01-12 15:41:14 +0200

    week 01 assignment

---
+++ scripts/getdates.js
@@ -0,0 +1,18 @@
+// DOM element for short date format
+const last = document.querySelector("#lastModified");
+const short = document.querySelector("#short");
+
+//Date object
+const today = new Date();
+
+{
+  datestyle: "Short";
+}
+last.innerHTML = ` Last Modified: <span class="date-format">${new Intl.DateTimeFormat(
+  "en-US",
+  {
+    dateStyle: "short",
+  }
+).format(today)}</span>`;
+
+short.innerHTML = `<span class="date-format">${today.getFullYear()}</span>`;
